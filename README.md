743f1a3bcfe3344525f80a66fb90e87b96966463
ryozu256 <ryozu256@gmail.com>
2016-06-24 17:32:00 +0900

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # user-scalable-no.js
+
+![Badge Status](https://ci-as-a-service)
+
+user-scalable=noの非対応ブラウザに対応するライブラリ
+
+## Description
+
+ios10ではアクセシビリティの観点で非対応になるuser-scalable=noに対応することができます。
+
+***DEMO:***
+
+![Demo](https://image-url.gif)
+
+## Usage
+
+1. jsのインクルード
+	<script src="js/user-scalable-no.js"></script>
+2. metaの追加
+	<meta testuoeuoeu>
+
+## Installation
+
+    $ git clone https://github.com/oha-ryoju/user-scalable-no.js
+
+## License
+
+[MIT](http://b4b4r07.mit-license.org)
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+gulp.task('js', function () {
+	gulp.src('./src/*.js')
+		.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))	
+		.pipe(babel({presets: ['es2015']}))
+        .pipe(uglify({preserveComments: 'some'}))
+        .pipe(rename('user-scalable-no.min.js'))
+		.pipe(gulp.dest('./'))
+});
+
+gulp.task('demo', function () {
+	gulp.src('./src/*.html')
+		.pipe(gulp.dest('./demo'))
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./src/*.js', ['js'])
+	gulp.watch('.src/*.html', ['demo'])
+});
+
+gulp.task('default', ['js', 'demo', 'watch']);
